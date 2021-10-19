893a98a9d405043b7d94f6ae0e34059af54cfe7f
anupprakash4 <88568799+anupprakash4@users.noreply.github.com>
2021-10-19 23:21:19 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,7 @@
 #! /usr/bin/python3
 # This Python program will print Hello World...
-print("Hello world....\n")
-print("Hello World Demo with GitHub Integration...\n")
-print("Hello World Demo with GitHub Integration...\n")
-print("Hello World Demo with GitHub Integration...\n")
-print("Hello World Demo with GitHub Integration...\n")
+print("Hello World Demo for GitHub hook trigger for GITscm polling...\n")
+print("Hello World Demo for GitHub hook trigger for GITscm polling...\n")
 
 
 
