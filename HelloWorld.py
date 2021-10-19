0bcb7405e4535cb181f2f077fd1801553a34ce85
anupprakash4 <88568799+anupprakash4@users.noreply.github.com>
2021-10-19 23:40:43 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python3
 # This Python program will print Hello World...
-print("Hello World Demo for GitHub hook trigger for GITscm polling...\n")
-print("Hello World Demo for GitHub hook trigger for GITscm polling...\n")
+print("Hello world demo for webhook Demo...\n")
+print("Hello world demo for webhook Demo...\n")
+print("Hello world demo for webhook Demo...\n")
 
 
 
