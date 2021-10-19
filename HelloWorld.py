fbdb26984cab051fa851815297c7b94a354ecab1
anupprakash4 <88568799+anupprakash4@users.noreply.github.com>
2021-10-19 23:01:22 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
-# This Phython program will print Hellow World...
+# This Python program will print Hello World...
+print("Hello world....\n")
 print("Hello World Demo with GitHub Integration...\n")
+
 
 
 
